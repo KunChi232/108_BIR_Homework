4dbbad9d9377340649f0cba43e13943d7df8a74b
KunChi <kuo23201199@gmail.com>
2019-10-18 00:39:30 +0800

    stem pubmed file

--- PubMed_WebCrawler/main.py
+++ PubMed_WebCrawler/main.py
@@ -2,15 +2,18 @@ import requests
 import xml.etree.ElementTree as ET
 import re
 import sys, os
-import time
+import time, nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 class PubMedParser:
     path = 'PubmedArticle/MedlineCitation/Article'
     index = dict()
+    ps = PorterStemmer()
     def __init__(self, fileContent):
         self.tree = ET.fromstring(fileContent)
         self.allArticle = self.getAllArticle(self.tree)
-        self.allTitles = self.parsingTitle(self.allArticle)
-        self.allContents, self.numOfWords, self.numOfCharacters = self.parsingContent(self.allArticle)
+        self.allTitles ,self.allTiles_stem= self.parsingTitle(self.allArticle)
+        self.allContents, self.allContents_stem, self.numOfWords, self.numOfWords_stem, self.numOfCharacters , self.numOfCharacters_stem= self.parsingContent(self.allArticle)
         self.allAuthorName = self.parsingAuthors(self.allArticle)
         # self.title_content_dictionary  = dict(zip(self.allTitles, self.allContents))
         # self.title_author_dictionary = dict(zip(self.allTitles, self.allAuthorName))
@@ -38,48 +41,72 @@ class PubMedParser:
                 self.index[word] = [num]
 
     def parsingContent(self,allArticle):
-        result = []
+        content = []
+        content_stem = []
         numOfWords = []
+        numOfWords_stem = []
         numOfCharacters = []
+        numOfCharacters_stem = []
         for article in allArticle:
             abstract = article.find('Abstract')
             if(abstract == None):
-                result.append('')
+                content.append('')
+                content_stem.append('')
                 numOfWords.append(0)
+                numOfWords_stem.append(0)
                 numOfCharacters.append(0)
+                numOfCharacters_stem.append(0)
             else:
                 abstractText = abstract.findall('AbstractText')
                 if(abstractText == None):
+                    content.append('')
+                    content_stem.append('')
                     numOfWords.append(0)
+                    numOfWords_stem.append(0)
                     numOfCharacters.append(0)
-                    result.append('')
+                    numOfCharacters_stem.append(0)
                 else:
                     s = ''
+                    _s = ''
                     words = 0
+                    words_stem = 0
                     chars = 0
+                    chars_stem = 0
                     for text in abstractText:
-                        self.createIndex(''.join(text.itertext()), len(result))
-                        words += len(re.findall(r'\w+',''.join(text.itertext())))
-                        chars += len(re.findall(r'\S',''.join(text.itertext())))
+                        fullText = ''.join(text.itertext())
+                        self.createIndex(fullText, len(content))
+                        words += len(re.findall(r'\w+', fullText))
+                        words_stem += len(word_tokenize(fullText))
+                        chars += len(re.findall(r'\S', fullText))
+                        chars_stem += len(re.findall(r'\w+', ' '.join(word_tokenize(fullText))))
                         if 'Label' in text.attrib:
-                            s+='<b>'+text.attrib['Label']+'</b>'+':<br>&nbsp&nbsp&nbsp&nbsp'+''.join(text.itertext())+' <br>'
+                            s+='<b>'+text.attrib['Label']+'</b>'+':<br>&nbsp&nbsp&nbsp&nbsp'+fullText+' <br>'
+                            _s+='<b>'+self.stemming(text.attrib['Label'])+'</b>'+':<br>&nbsp&nbsp&nbsp&nbsp'+self.stemming(fullText)+' <br>'
                         else:
-                            s+=' '+'<br>&nbsp&nbsp&nbsp'+''.join(text.itertext())+'\n'
+                            s+=' '+'<br>&nbsp&nbsp&nbsp'+fullText
+                            _s+=' '+'<br>&nbsp&nbsp&nbsp'+self.stemming(fullText)
                     numOfWords.append(words)
+                    numOfWords_stem.append(words_stem)
                     numOfCharacters.append(chars)
-                    result.append(s)
-        return result, numOfWords, numOfCharacters
+                    numOfCharacters_stem.append(chars_stem)
+                    content.append(s)
+                    content_stem.append(_s)
+        # print(content_stem)
+        return content, content_stem, numOfWords, numOfWords_stem, numOfCharacters, numOfCharacters_stem
 
     def parsingTitle(self,allArticle):
-        result = []
+        title = []
+        title_stem = []
         for article in allArticle:
             articleTitle = article.find('ArticleTitle')
             if(articleTitle == None or articleTitle.text == None):
-                result.append('')
+                title.append('')
+                title_stem.append('')
             else:
-                self.createIndex(str(articleTitle.text), len(result))
-                result.append(articleTitle.text)
-        return result
+                self.createIndex(str(articleTitle.text), len(title))
+                title.append(articleTitle.text)
+                title_stem.append(self.stemming(articleTitle.text))
+        return title, title_stem
 
     def parsingAuthors(self,allArticle):
         result = []
@@ -102,40 +129,58 @@ class PubMedParser:
     def mark_content(self,position, string):
         for num, tup in enumerate(position):
             string = string[:tup[0] + num*23] + self.color.RED_START + string[tup[0] + num*23:tup[1] + num*23] + self.color.END + string[tup[1] + num*23:]
+        print(string)
         return string
 
     def match(self,query):
+        titles = list()
+        titles_stem = list()
+        contents = list()
+        contents_stem = list()
+        authors = list()
+        numOfCharacters = list()
+        numOfCharacters_stem = list()
+        numOfWords = list()
+        numOfWords_stem = list()
+        union = list()
+
+
         start_time = time.time()
         query = query.split(' ')
         query_string = ''
+        query_string_stem = ''
         for q in query:
             query_string += '(' + q + ')|'
+            query_string_stem += '(' + self.stemming(q) + ')|'
         query_string = query_string[:-1]
-        titles = list()
-        contents = list()
-        authors = list()
-        numOfCharacters = list()
-        numOfWords = list()
-        words_times_pair = list()
-        union = list()
+        query_string_stem = query_string_stem[:-1]
         for q in query:
             try:
                 union = list(set(union) | set(self.index[q.lower()]))
             except KeyError as e:
                 print(e)
-        words_times_pair = (self.countAllWordsTimes(union))
+        words_times_pair , word_stem_times_pair= (self.countAllWordsTimes(union))
                 
         for i in union:
             titleIterator = re.finditer(query_string, self.allTitles[i], re.IGNORECASE)
+            titleStemIterator = re.finditer(query_string_stem, self.allTiles_stem[i], re.IGNORECASE)
             contentIterator = re.finditer(query_string, self.allContents[i], re.IGNORECASE)
+            contentStemIterator = re.finditer(query_string_stem, self.allContents_stem[i], re.IGNORECASE)
+
             queryPosInTitle = [(m.start(), m.end()) for m in titleIterator]
             queryPosInContent = [(m.start(), m.end()) for m in contentIterator]
+            queryPosInTitle_stem = [(m.start(), m.end()) for m in titleStemIterator]
+            queryPosInContent_stem = [(m.start(), m.end()) for m in contentStemIterator]
             if(queryPosInTitle or queryPosInContent):
                 titles.append(self.allTitles[i])
+                titles_stem.append(self.allTiles_stem[i])
                 numOfWords.append(self.numOfWords[i])
+                numOfWords_stem.append(self.numOfWords_stem[i])
                 numOfCharacters.append(self.numOfCharacters[i])
+                numOfCharacters_stem.append(self.numOfCharacters_stem[i])
                 # allWordsTimes = self.countAllWordsTimes(self.allContents[i], allWordsTimes)
                 contents.append(self.mark_content(queryPosInContent, self.allContents[i]))
+                contents_stem.append(self.mark_content(queryPosInContent_stem, self.allContents_stem[i]))
                 authors.append(','.join(self.allAuthorName[i]))
         comm = []
         for i, content in enumerate(contents):
@@ -147,28 +192,40 @@ class PubMedParser:
                     break
         end_time = time.time() - start_time
         print(end_time)
-        print(words_times_pair)
-        return titles, contents ,authors, numOfWords, numOfCharacters, comm, words_times_pair
+        return titles, titles_stem, contents , contents_stem, authors, numOfWords, numOfWords_stem, numOfCharacters, numOfCharacters_stem, comm, words_times_pair, word_stem_times_pair
+
+    def stemming(self, string):
+        words = word_tokenize(string)
+        reslut = list()
+        for word in words:
+            reslut.append(self.ps.stem(word))
+        return ' '.join(reslut)
 
     def countAllWordsTimes(self, union):
         words_times_dict = dict()
+        words_stem_times_dict = dict()
         tags = ['<br>', '</br>', '&nbsp', '<b>', '</b>']
         for i in union:
             content = self.allContents[i]
+            content_stem = self.allContents_stem[i]
             for tag in tags:
                 content = content.replace(tag, '')
+                content_stem = content_stem.replace(tag, '')
             extract = re.findall(r'\w+', str(content))
+            extract_stem = re.findall(r'\w+', str(content_stem))
             for word in extract:
                 word = word.lower()
                 count = words_times_dict.get(word, 0)
                 words_times_dict[word] = count + 1
-        print(words_times_dict)
-        return self.separate_dict_sotd(words_times_dict)
+            for word in extract_stem:
+                word = word.lower()
+                count = words_stem_times_dict.get(word, 0)
+                words_stem_times_dict[word] = count + 1
+        return self.separate_dict_sotd(words_times_dict), self.separate_dict_sotd(words_stem_times_dict)
 
     def separate_dict_sotd(self,words_times_dict):
         pairs = list(words_times_dict.items())
         pairs.sort(key = lambda tup:tup[1], reverse=True)
-        print(pairs)
         return pairs
 
     class color:
