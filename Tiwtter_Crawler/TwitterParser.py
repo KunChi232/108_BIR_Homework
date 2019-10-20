8ef142fc992f8ddb54917b0918c62d914dc52439
KunChi <kuo23201199@gmail.com>
2019-10-20 13:08:17 +0800

    create index in twitter

--- Tiwtter_Crawler/TwitterParser.py
+++ Tiwtter_Crawler/TwitterParser.py
@@ -2,16 +2,24 @@ import os
 import json
 import re
 class TwitterParser:
+    index = dict()
     def __init__(self, fileContent):
-        # for i in range(self.numOfFolder):
-        #     for file in os.listdir(self.file_path + '0' + str(i) + '/'):
-        #         if file.endswith('.json'):
-        #             lines = [line for line in open(self.file_path + '0' + str(i) + '/' + file)]
-        # for j in [json.loads(line) for line in lines]:
-        #     self.jsonFiles.append(j)
         self.jsonFiles = json.loads(fileContent)
-        print(len(self.jsonFiles))
+        self.createIndex(self.jsonFiles)
+        print(self.index)
     
+    def createIndex(self, jsonFiles):
+        for tweet in jsonFiles:
+            if('text' in tweet):
+                text = tweet['text']
+                extract = re.findall(r'\w+', text, re.IGNORECASE)
+                for word in extract:
+                    word = word.lower()
+                    count = self.index.get(word, 0)
+                    self.index[word] = count +1
+
+
+
     def mark_content(self, position, string):
         for num, tup in enumerate(position):
             string = string[:tup[0] + num*23] + self.color.RED_START + string[tup[0] + num*23:tup[1] + num*23] + self.color.END + string[tup[1] + num*23:]
@@ -22,6 +30,7 @@ class TwitterParser:
             key = tag.split(' ')[1]
             string = string.replace(tag,'<a href=https://twitter.com/search?q=%23'+key+'&src=typd>'+tag+'</a>')
         return string
+
     def match(self, query):
         query_string = ''
         for q in query.split(' '):
@@ -41,7 +50,6 @@ class TwitterParser:
                 iterTag = re.findall('(#[a-z\d-]+|# [a-z\d-]+)', text, re.IGNORECASE)
                 if(iterTag):
                     text = self.mark_hashtag(iterTag, text)
-                print(text)
                 iterator = re.finditer(query_string, text, re.IGNORECASE)
                 position = [(m.start(), m.end()) for m in iterator]
                 if(position):
@@ -49,7 +57,6 @@ class TwitterParser:
                     user_names.append(tweet['author_id'])
                     screen_names.append('@')
                     created_ats.append(tweet['formatted_date'].replace('+0000 ',''))
-        print(texts)
         return texts, user_names, screen_names,created_ats, numOfWords, numOfCharacters
 
     class color:
